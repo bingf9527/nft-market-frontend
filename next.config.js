b2f3e5af3123451463b2ea68b851b2c070b4defa
fangbing9527 <fangbing9527@gmail.com>
2024-04-09 17:38:02 +0800

    cors

--- next.config.js
+++ next.config.js
@@ -2,16 +2,3 @@
 const nextConfig = {}
 
 module.exports = nextConfig
-next.config.js
-// const { createProxyMiddleware } = require('http-proxy-middleware');
-
-// module.exports = {
-//   async rewrites() {
-//     return [
-//       {
-//         source: '/api/:path*',
-//         destination: 'http://118.31.71.213:8080/:path*' // 目标API地址
-//       },
-//     ]
-//   },
-// }
